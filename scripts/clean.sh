9422839f57792f6307efa29dc22cee5ec3e982ad
Oleksandr <gavrylenko.aleksandr@gmail.com>
2025-05-06 14:31:16 +0200

    Updated Processor.

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -3,4 +3,5 @@
 #docker exec broker-1 ./opt/kafka/bin/kafka-consumer-groups.sh --bootstrap-server broker-1:19092,broker-2:19092,broker-3:19092 --delete --group subreddits-group-1
 docker exec broker-1 ./opt/kafka/bin/kafka-topics.sh --bootstrap-server broker-1:19092,broker-2:19092,broker-3:19092 --delete --topic history
 docker exec broker-1 ./opt/kafka/bin/kafka-topics.sh --bootstrap-server broker-1:19092,broker-2:19092,broker-3:19092 --delete --topic dlq
+docker exec broker-1 ./opt/kafka/bin/kafka-topics.sh --bootstrap-server broker-1:19092,broker-2:19092,broker-3:19092 --delete --topic domain-count
 docker exec broker-1 ./opt/kafka/bin/kafka-topics.sh --bootstrap-server broker-1:19092,broker-2:19092,broker-3:19092 --list
