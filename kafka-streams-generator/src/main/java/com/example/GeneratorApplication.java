c1446138e2f5cc5c7f05cba49962c781bc9957c3
Oleksandr <gavrylenko.aleksandr@gmail.com>
2025-05-07 18:25:52 +0200

    Refactored Producer.

--- kafka-streams-generator/src/main/java/com/example/GeneratorApplication.java
+++ kafka-streams-generator/src/main/java/com/example/GeneratorApplication.java
@@ -1,28 +1,53 @@
 package com.example;
 
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
 
-import java.util.Properties;
+import java.util.HashMap;
+import java.util.Map;
 
-import static org.apache.kafka.clients.CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG;
-import static org.apache.kafka.clients.producer.ProducerConfig.*;
 import static org.apache.kafka.clients.producer.ProducerConfig.ACKS_CONFIG;
+import static org.apache.kafka.clients.producer.ProducerConfig.CLIENT_ID_CONFIG;
 
-/**
- * @author Oleksandr Havrylenko
- **/
+@SpringBootApplication
 public class GeneratorApplication {
-    public static void main(String[] args) {
-        final Properties producerProperties = new Properties() {{
-            put(BOOTSTRAP_SERVERS_CONFIG, System.getenv()
-                    .getOrDefault("BOOTSTRAP_SERVERS", "localhost:29092, localhost:39092, localhost:49092"));
-            put(KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-            put(VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-            put(CLIENT_ID_CONFIG, System.getenv().getOrDefault("CLIENT_ID", "history-producer"));
-            put(ACKS_CONFIG, System.getenv().getOrDefault("ACKS", "1"));
-        }};
 
-        final ProducerHistory producerHistory = new ProducerHistory(producerProperties);
-        producerHistory.produce();
+    public static void main(String[] args) {
+        SpringApplication.run(GeneratorApplication.class, args);
+    }
+
+    @Bean
+    public ApplicationRunner runner(HistoryProducer historyProducer) {
+        return args -> {
+            historyProducer.produce();
+        };
+    }
+
+    @Bean
+    public ProducerFactory<String, String> producerFactory() {
+        return new DefaultKafkaProducerFactory<>(senderProps());
+    }
+
+    private Map<String, Object> senderProps() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, System.getenv()
+                .getOrDefault("BOOTSTRAP_SERVERS", "localhost:29092, localhost:39092, localhost:49092"));
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(CLIENT_ID_CONFIG, System.getenv().getOrDefault("CLIENT_ID", "history-producer"));
+        props.put(ACKS_CONFIG, System.getenv().getOrDefault("ACKS", "1"));
+        return props;
+    }
+
+    @Bean
+    public KafkaTemplate<String, String> kafkaTemplate(ProducerFactory<String, String> producerFactory) {
+        return new KafkaTemplate<>(producerFactory);
     }
 }
